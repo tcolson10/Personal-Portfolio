dcd861a7ed28ed157e12767f37d7b2064f219183
tcolson10 <trevor@colson.dev>
2025-06-02 12:52:54 -0600

    updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div name='about' className='w-full h-screen bg-gray-900 text-gray-400'>
-            <div className='flex flex-col justify-center items-center w-full h-full'>
-                <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-                    <div className='sm:text-right pb-8 pl-4'>
-                        <p className='text-4xl font-bold inline border-b-4 text-white border-[#4285F4]'>About</p>
-                    </div>
-                    <div></div>
-                </div>
-                <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-                    <div className='sm:text-right text-4xl font-bold'>
-                        <p className='text-gray-500'>Hi. I'm Trevor, nice to meet you. Please take a look around.</p>
-                    </div>
-                    <div>
-                        <p className='text-white text-xl'>
-                            I'm Trevor Colson, I live in Provo, Utah, and I am currently pursuing my degree in Information Systems at the University of Utah.
-                            I have always had a passion for technology and I am an aspiring software engineer. I've had the privilege of working as a full-time web developer,
-                            and hope to be able to continue my journey in the world of tech. I am looking for internships for the Summer of 2024 as well as any other type of related job opportunities.
-                        </p>
+	return (
+		<div name="about" className="w-full h-screen bg-gray-900 text-gray-400">
+			<div className="flex flex-col justify-center items-center w-full h-full">
+				<div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+					<div className="sm:text-right pb-8 pl-4">
+						<p className="text-4xl font-bold inline border-b-4 text-white border-[#4285F4]">
+							About
+						</p>
+					</div>
+					<div></div>
+				</div>
+				<div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+					<div className="sm:text-right text-4xl font-bold">
+						<p className="text-gray-500">
+							Hi. I'm Trevor, nice to meet you. Please take a look around.
+						</p>
+					</div>
+					<div>
+						<p className="text-white text-xl">
+							I'm Trevor Colson, I live in Salt Lake City, Utah, and I recently
+							graduated with my degree in Information Systems from the
+							University of Utah. I have always had a passion for technology and
+							I am a software engineer with professional experience working with
+							the Utah Jazz and Mammoth as well as with Optix Solutions. Through
+							these roles, I've gained valuable experience in web development
+							and software engineering, and I'm excited to continue growing in
+							the tech industry. I am actively seeking new opportunities and
+							full-time positions where I can contribute my skills and continue
+							learning.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-                    </div>
-                </div>
-            </div >
-        </div>
-
-    )
-}
-
-export default About
+export default About;
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,66 +1,91 @@
-import React from 'react'
-import workImg from '../assets/realestate.jpg'
+import React from "react";
+import workImg from "../assets/realestate.jpg";
 
 function Experience() {
-    return (
-        <div name='work' className='w-ful md:h-screen bg-gray-900 text-gray-300'>
-            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-                <div className='pb-8'>
-                    <p className='text-4xl font-bold inline border-b-4 text-white border-[#4285F4]'>Work</p>
-                    <p className='py-6 text-gray-500 text-2xl'>Check out some of my recent work</p>
-                </div>
+	return (
+		<div name="work" className="w-full md:h-screen bg-gray-900 text-gray-300">
+			<div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+				<div className="pb-8">
+					<p className="text-4xl font-bold inline border-b-4 text-white border-[#4285F4]">
+						Work
+					</p>
+					<p className="py-6 text-gray-500 text-2xl">
+						Check out some of my recent work
+					</p>
+				</div>
 
+				<div className="grid sm:grid-cols-2 gap-4">
+					{/* Shelby Floral */}
+					<div
+						style={{ backgroundImage: `url(/shelbyFloral.png)` }}
+						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+					>
+						<div className="opacity-0 group-hover:opacity-100">
+							<div>
+								<span className="text-2xl font-bold text-white tracking-wider">
+									Shelby Floral
+								</span>
+								<div className="pt-8 text-center">
+									<a
+										href="https://shelby-floral.com"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+											Demo
+										</button>
+									</a>
+									<a
+										href="https://github.com/tcolson10"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+											Code
+										</button>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
 
-                {/* 
-
-                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-
-                    
-
-                    <div style={{ backgroundImage: `url(${workImg})` }} className=' shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-                        
-
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <div>
-                                <span className='text-2xl font-bold text-white tracking-wider'>
-                                    React JS Application
-                                </span>
-                                <div className='pt-8 text-center'>
-                                    <a href='/'>
-                                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                                    </a>
-                                    <a href='/'>
-                                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div style={{ backgroundImage: `url(${workImg})` }} className=' shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-                        
-
-                        <div className='opacity-0 group-hover:opacity-100'>
-                            <div>
-                                <span className='text-2xl font-bold text-white tracking-wider'>
-                                    React JS Application
-                                </span>
-                                <div className='pt-8 text-center'>
-                                    <a href='/'>
-                                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                                    </a>
-                                    <a href='/'>
-                                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>  */}
-            </div>
-        </div>
-    )
+					{/* Monthly Blooms */}
+					<div
+						style={{ backgroundImage: `url(/monthlyBlooms.png)` }}
+						className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+					>
+						<div className="opacity-0 group-hover:opacity-100">
+							<div>
+								<span className="text-2xl font-bold text-white tracking-wider">
+									Monthly Blooms
+								</span>
+								<div className="pt-8 text-center">
+									<a
+										href="https://monthlyblooms.com"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+											Demo
+										</button>
+									</a>
+									<a
+										href="https://github.com/tcolson10"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+											Code
+										</button>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Experience
+export default Experience;
